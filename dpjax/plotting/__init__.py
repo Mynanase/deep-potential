134af384b9ca29bb8e4de80646dc4481611dfdd4
qttao <qttao@outlook.com>
2026-07-26 00:40:38 +0800

    Revert "Add acceleration-CBE diagnostics for Halo DF scores"
    This reverts commit 4107c58a372c1301394ae55145c38ec43baa2321.

--- dpjax/plotting/__init__.py
+++ dpjax/plotting/__init__.py
@@ -1,6 +1,5 @@
 from .flow_projections import calc_coords, plot_1d_marginals, plot_2d_marginal
 from .training_curves import plot_df_training, plot_phi_training, read_metrics
-from .auriga_score import plot_truth_cbe_diagnostics
 from .diagnostics import (
     plot_phase_space_grid,
     plot_score_comparison,
